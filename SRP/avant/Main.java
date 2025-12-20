a035fc225d5fb6683d19386ebd660c6af0714b6d
laeticia <laeticiafeudjiotsague@gmail.com>
2025-12-20 11:16:08 +0100

    principe srp avant refactoring

---
+++ SRP/avant/Main.java
@@ -0,0 +1,12 @@
+package SRP.avant;
+
+// Utilisation de la classe Book
+public class Main {
+
+    public static void main(String[] args) {
+        Book book = new Book("Les principes SOLID", "Etudiants de M1-GL", "Révision des principes SOLID");
+        book.printToScreen();
+        book.saveToDatabase();
+        book.emprunter("Marcial");
+    }
+}
